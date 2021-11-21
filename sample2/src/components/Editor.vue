28e2b7250d3a3e6b7e2679790ea1cf012e2deb1a
monchi3 <monchigamemode@gmail.com>
2021-11-21 21:09:10 +0900

    Vue CLI2

--- sample2/src/components/Editor.vue
+++ sample2/src/components/Editor.vue
@@ -5,27 +5,35 @@
 <script>
 import ace from "ace-builds";
 import "ace-builds/webpack-resolver";
-
 // 使いたい言語モードのインポート
 import "ace-builds/src-noconflict/mode-javascript";
-
 // 使いたいテーマのインポート
 import "ace-builds/src-noconflict/theme-github";
 export default {
   name: "Ace",
+  props: ["editorId", "content"],
   data() {
     return {
       editor: Object,
+      beforeContent: "",
     };
   },
   mounted() {
     this.editor = ace.edit(this.$refs.ace);
     this.editor.session.setMode("ace/mode/javascript");
     this.editor.setTheme("ace/theme/monokai");
+    this.editor.on("change", () => {
+      this.beforeContent = this.editor.getValue();
+      this.$emit("change-content", this.editor.getValue());
+    });
   },
-  watch:{
-      
-  }
+  watch: {
+    'content'(value){
+      if(this.beforeContent!==value){
+        this.editor.setValue(value, 1)
+      }
+    }
+  },
 };
 </script>
 
--- sample2/src/views/Home.vue
+++ sample2/src/views/Home.vue
@@ -2,12 +2,6 @@
   <div>
     <v-row>
       <v-col cols="12" sm="6" class="py-2">
-        <v-btn v-on:click="loadFile" outlined>
-          <v-icon>mdi-folder-open-outline</v-icon>
-        </v-btn>
-        <v-btn v-on:click="saveFile" outlined>
-          <v-icon>mdi-content-save-edit</v-icon>
-        </v-btn>
         <v-btn v-on:click="compile" outlined>
           <v-icon>mdi-play-circle-outline</v-icon>
         </v-btn>
@@ -16,9 +10,8 @@
     <div style="height: 200px">
       <editor
         editor-id="editorA"
-        :content="contentA"
-        v-on:change-content="changeContentA"
-        @parentMethod="handEditor"
+        content="contentA"
+        v-on:change-content="changeContent"
       >
       </editor>
     </div>
@@ -45,11 +38,69 @@
 </template>
 
 <script>
-import Editor from '../components/Editor'
+import Editor from "../components/Editor";
 export default {
-    components:{
-        Editor
-    }
+  components: {
+    Editor,
+  },
+  data() {
+    return {
+      roomName: "",
+      content: "",
+    };
+  },
+  computed: {
+    clock: function () {
+      return new Date();
+    },
+    now: function () {
+      return (
+        this.clock.getHours() +
+        ":" +
+        this.clock.getMinutes() +
+        ":" +
+        this.clock.getSeconds()
+      );
+    },
+    fusionString: function () {
+      return this.content + ":" + this.now;
+    },
+  },
+  methods: {
+    compile: function () {
+      
+    },
+    login: function () {
+      ws.send(this.roomName);
+    },
+    changeContent(val) {
+      if (this.content !== val) {
+        this.content = val;
+        ws.send(this.fusionString);
+      }
+    },
+  },
+};
+
+var ws = new WebSocket("ws://localhost:10005");
+
+// 接続
+ws.onopen = function () {
+  console.log("onopen");
+};
+
+ws.onmessage = function (e) {
+  // e.data contains received string.
+  console.log("onmessage: " + e.data);
+};
+
+ws.onclose = function () {
+  console.log("onclose");
+};
+
+ws.onerror = function (e) {
+  console.log("onerror");
+  console.log(e);
 };
 </script>
 
